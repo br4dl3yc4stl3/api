3d8b4a79267880eafb75594addce528717474415
br4dl3yc4stl3 <47078776+br4dl3yc4stl3@users.noreply.github.com>
2019-02-25 21:26:18 +0900

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -2,6 +2,22 @@
 
 (function() {
 	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
-	var apiKey = "APIKEY"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
-    var httpRequest;
+	var apiKey = "4d0867c01cdc7c75e105b97051855daf"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+	var httpRequest;
+	makeRequest();
+	
+	// create and send XHR request
+	function makeRequest() {
+		httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = responseMethod;
+		httpRequest.open('GET', url + '&appid=' + apiKey);
+		httpRequest.send();
+	}
+
+	//handle XHR response
+	function responseMethod() {
+		if (httpRequest.readyState ===4) {
+			console.log(httpRequest.responseText);
+		}
+	}
 })();

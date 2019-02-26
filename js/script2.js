3deac59739f80dd1fc832196c4a0cdf446ae7de0
br4dl3yc4stl3 <47078776+br4dl3yc4stl3@users.noreply.github.com>
2019-02-26 20:44:50 +0900

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,40 @@
 "use strict";
 
-// Fetch request
+// jQuery request
 (function() {
+	var url = "https://api.openweathermap.org/data/2.5/weather?q=London,England";
+	var apiKey = "4d0867c01cdc7c75e105b97051855daf"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+
+	$.get(url +'&appid=' + apiKey).done(function(response) {
+//		console.log(response);
+		updateUISuccess(response);
+	}).fail(function(error) {
+
+	});
+
+	//handle XHR success
+	function updateUISuccess(response) {
+		var condition = response.weather[0].main;
+		var degC = response.main.temp - 273.15;
+		var degCInt = Math.floor(degC);
+		var degF = degC * 1.8 + 32;
+		var degFInt = Math.floor(degF);
+//		var weatherBox = document.getElementById("weather");
+//		weatherBox.innerHTML = "<p>" + degCInt + "&#176; C / " + degFInt + "&#176; F</p><p>" + condition + "</p>"
+		var $weatherBox = $('#weather');
+		$weatherBox.append("<p>" + degCInt + "&#176; C / " + degFInt + "&#176; F</p><p>" + condition + "</p>");
+	}
+	//handle XHR error
+	function updateUIError() {
+//		var weatherBox = document.getElementById("weather");
+//		weatherBox.className = "hidden";
+		var $weatherBox = $('#weather');
+		$weatherBox.addClass('hidden');
+	}
+})();
+
+// Fetch request
+/*(function() {
 	var url = "https://api.openweathermap.org/data/2.5/weather?q=London,England";
 	var apiKey = "4d0867c01cdc7c75e105b97051855daf"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
 
@@ -32,3 +65,52 @@
 		weatherBox.className = "hidden";
 	}
 })();
+*/
+
+// XHR request
+/*
+(function() {
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=London,England";
+	var apiKey = "4d0867c01cdc7c75e105b97051855daf"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+	var httpRequest;
+	makeRequest();
+	
+	// create and send XHR request
+	function makeRequest() {
+		httpRequest = new XMLHttpRequest();
+		httpRequest.onreadystatechange = responseMethod;
+		httpRequest.open('GET', url + '&appid=' + apiKey);
+		httpRequest.send();
+	}
+
+	//handle XHR response
+	function responseMethod() {
+		if (httpRequest.readyState ===4) {
+			if (httpRequest.status === 200) {
+				updateUISuccess(httpRequest.responseText);
+			} else {
+				updateUIError();
+			}
+			console.log(httpRequest.responseText);
+		}
+	}
+
+	//handle XHR success
+	function updateUISuccess(responseText) {
+		var response = JSON.parse(responseText);
+		var condition = response.weather[0].main;
+		var degC = response.main.temp - 273.15;
+		var degCInt = Math.floor(degC);
+		var degF = degC * 1.8 + 32;
+		var degFInt = Math.floor(degF);
+		var weatherBox = document.getElementById("weather");
+		weatherBox.innerHTML = "<p>" + degCInt + "&#176; C / " + degFInt + "&#176; F</p><p>" + condition + "</p>"
+	}
+
+	//handle XHR error
+	function updateUIError() {
+		var weatherBox = document.getElementById("weather");
+		weatherBox.className = "hidden";
+	}
+})();
+*/

1ac4ecb3c08af4bfd7f9644b895ff42ec1016857
br4dl3yc4stl3 <47078776+br4dl3yc4stl3@users.noreply.github.com>
2019-02-26 19:39:15 +0900

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,34 +1,23 @@
 "use strict";
 
+// Fetch request
 (function() {
 	var url = "https://api.openweathermap.org/data/2.5/weather?q=London,England";
 	var apiKey = "4d0867c01cdc7c75e105b97051855daf"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
-	var httpRequest;
-	makeRequest();
-	
-	// create and send XHR request
-	function makeRequest() {
-		httpRequest = new XMLHttpRequest();
-		httpRequest.onreadystatechange = responseMethod;
-		httpRequest.open('GET', url + '&appid=' + apiKey);
-		httpRequest.send();
-	}
 
-	//handle XHR response
-	function responseMethod() {
-		if (httpRequest.readyState ===4) {
-			if (httpRequest.status === 200) {
-				updateUISuccess(httpRequest.responseText);
-			} else {
-				updateUIError();
-			}
-			console.log(httpRequest.responseText);
+	fetch(url + '&appid=' + apiKey).then(function(response) {
+		if (!response.ok) {
+			throw Error(response.statusText);
 		}
-	}
+		return response.json();
+	}).then(function() {
+		updateUISuccess(response);
+	}).catch(function(error) {
+		updateUIError();
+	});
 
 	//handle XHR success
-	function updateUISuccess(responseText) {
-		var response = JSON.parse(responseText);
+	function updateUISuccess(response) {
 		var condition = response.weather[0].main;
 		var degC = response.main.temp - 273.15;
 		var degCInt = Math.floor(degC);
@@ -37,7 +26,6 @@
 		var weatherBox = document.getElementById("weather");
 		weatherBox.innerHTML = "<p>" + degCInt + "&#176; C / " + degFInt + "&#176; F</p><p>" + condition + "</p>"
 	}
-
 	//handle XHR error
 	function updateUIError() {
 		var weatherBox = document.getElementById("weather");
